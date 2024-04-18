b89670bcf21c0273d1b5835cea0c18ee42989b4f
Neha <95375309+02Neha@users.noreply.github.com>
2024-04-18 20:52:09 +0530

    Add files via upload

---
+++ Gym-Routine-Tracker-Watch-App-main/Gym-Routine-Tracker-Watch-App-main/Sources/WatchApp.swift
@@ -0,0 +1,49 @@
+//
+//  WatchApp.swift
+//
+// Copyright 2022, 2023  OpenAlloc LLC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import os
+import SwiftUI
+
+import GroutLib
+import TrackerLib
+
+@main
+struct Watch_App: App {
+    @Environment(\.scenePhase) var scenePhase
+
+    // MARK: - Locals
+
+    private let coreDataStack = CoreDataStack(isCloud: true)
+
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!,
+                                category: "App")
+
+    // MARK: - Scene
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext,
+                             coreDataStack.container.viewContext)
+                .environmentObject(coreDataStack)
+        }
+        .onChange(of: scenePhase) { _ in
+            // save if transitioning to inactive or background
+            guard scenePhase == .inactive || scenePhase == .background else { return }
+
+            // and changes are pending
+            do {
+                try coreDataStack.container.viewContext.save()
+            } catch {
+                logger.error("\(#function): \(error.localizedDescription)")
+            }
+        }
+    }
+}
